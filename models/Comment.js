86fbf78fd7147eee7826bed5702defd28c12739c
Logan McDaniel <lsmcd@pm.me>
2024-04-14 22:06:06 -0600

    add database models

---
+++ models/Comment.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection")
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        private: {
+            type: DataTypes.BOOLEAN
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "User"
+            }
+        }
+    },
+    {
+        sequelize,
+    }
+);
+
+module.exports = Comment;
---
+++ models/Post.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection")
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        sport: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        distance: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        duration: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        private: {
+            type: DataTypes.BOOLEAN
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "User"
+            }
+        }
+    },
+    {
+        sequelize,
+    }
+);
+
+module.exports = Post;
