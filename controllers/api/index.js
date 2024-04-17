9a2677b47b3e6af5876c23d4e1174e47821ae45c
Logan McDaniel <lsmcd@pm.me>
2024-04-17 17:31:14 -0600

    add last api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const followRoutes = require("./followRoutes");
 const postRoutes = require("./postRoutes");
+const commentRoutes = require("./commentRoutes");
 
 router.use("/user", userRoutes);
 router.use("/follow", followRoutes);
 router.use("/post", postRoutes);
+router.use("/comment", commentRoutes);
 
 module.exports = router;
