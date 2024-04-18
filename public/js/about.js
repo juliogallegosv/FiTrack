8a5ad75bad9418086232c5194122e7af4cd34805
m13corona <mcorona1308@gmail.com>
2024-04-18 08:19:10 -0600

    add functionality to templates and js

---
+++ public/js/about.js
@@ -0,0 +1,37 @@
+const aboutFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const description = document.querySelector('#user-description').value.trim();
+    const units = document.querySelector('#units').value.trim();
+    const gender = document.querySelector('#user-gender').value.trim();
+    const country = document.querySelector('#user-country').value.trim();
+    
+
+    if (description && units && gender && country) {
+      const response = await fetch(`/api/about`, { //CHECK IF CORRECT
+        method: 'POST',
+        body: JSON.stringify({ description, units, gender, country}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+console.log(gender)
+
+
+      if (response.ok) {
+        document.location.replace('/about');
+      } else {
+        alert('About section filled incorrectly');
+      }
+    }
+  };
+  
+console.log(gender)
+  
+  document
+    .querySelector('.about-form')
+    .addEventListener('submit', aboutFormHandler);
+  
+  
+  
